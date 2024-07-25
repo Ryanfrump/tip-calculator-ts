712a5fead2bafde27e2f043bd7c659d0f4714f57
Ryanfrump <ryanfrump@gmail.com>
2024-07-25 10:27:13 -0600

    add functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import Calculator from "./Calculator";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <header>
+        <h1>Tip Calculator</h1>
+        <p>
+          Given your bill amount and your tip percentage I will give you tyour
+          total bill
+        </p>
+      </header>
+      <main className="main-container">
+        <Calculator />
+      </main>
+    </>
+  );
 }
 
 export default App;
---
+++ src/Calculator.tsx
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+
+const Calculator: React.FC = () => {
+  const [billAmount, setBillAmount] = useState<number | string>("");
+  const [tipAmount, setTipAmount] = useState<number | string>("");
+  const [totalBill, setTotalBill] = useState<number | null>(null);
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault(); // Stops page from refreshing
+
+    const parsedBillAmount = parseFloat(billAmount.toString());
+    const parsedTipPercentage = parseFloat(tipAmount.toString());
+
+    if (!isNaN(parsedBillAmount) && !isNaN(parsedTipPercentage)) {
+      setTotalBill(
+        parsedBillAmount + (parsedBillAmount * parsedTipPercentage) / 100
+      );
+    } else {
+      setTotalBill(null);
+    }
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit} className="form-contanier">
+        <div className="amount-container">
+          <label htmlFor="amount">Bill Amount</label>
+          <input
+            type="number"
+            id="bill-amount"
+            placeholder="What was your bill amount"
+            value={billAmount !== undefined ? billAmount : ""}
+            onChange={(event: React.ChangeEvent<HTMLInputElement>): void =>
+              setBillAmount(event.target.value)
+            }
+          />
+        </div>
+        <div className="tip-container">
+          <label htmlFor="tip">Tip Amount</label>
+          <input
+            type="number"
+            id="tip-amount"
+            placeholder="Enter your tip amount"
+            value={tipAmount !== undefined ? tipAmount : ""}
+            onChange={(event: React.ChangeEvent<HTMLInputElement>): void =>
+              setTipAmount(event.target.value)
+            }
+          />
+        </div>
+        <div className="button-container">
+          <button type="submit">Calculate Bill</button>
+        </div>
+      </form>
+      {totalBill !== null && (
+        <div>
+          <h2>your bill is {totalBill.toFixed(2)}$</h2>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Calculator;
